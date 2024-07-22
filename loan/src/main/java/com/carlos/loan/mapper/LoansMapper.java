54edb75664e7e5029227403eee3490e631ad4c20
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-07-22 08:22:36 -0500

    Loand and Cards

---
+++ loan/src/main/java/com/carlos/loan/mapper/LoansMapper.java
@@ -0,0 +1,27 @@
+package com.carlos.loan.mapper;
+
+import com.carlos.loan.dto.LoansDto;
+import com.carlos.loan.entity.Loans;
+
+public class LoansMapper {
+
+    public static LoansDto mapToLoansDto(Loans loans, LoansDto loansDto) {
+        loansDto.setLoanNumber(loans.getLoanNumber());
+        loansDto.setLoanType(loans.getLoanType());
+        loansDto.setMobileNumber(loans.getMobileNumber());
+        loansDto.setTotalLoan(loans.getTotalLoan());
+        loansDto.setAmountPaid(loans.getAmountPaid());
+        loansDto.setOutstandingAmount(loans.getOutstandingAmount());
+        return loansDto;
+    }
+
+    public static Loans mapToLoans(LoansDto loansDto, Loans loans) {
+        loans.setLoanNumber(loansDto.getLoanNumber());
+        loans.setLoanType(loansDto.getLoanType());
+        loans.setMobileNumber(loansDto.getMobileNumber());
+        loans.setTotalLoan(loansDto.getTotalLoan());
+        loans.setAmountPaid(loansDto.getAmountPaid());
+        loans.setOutstandingAmount(loansDto.getOutstandingAmount());
+        return loans;
+    }
+}
